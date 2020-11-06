a3c461a55d4d27c2e9fc2d3ba8b764a48c54397e
baswebapp <tommy15000260694@dingtalk.com>
2020-11-06 12:51:02 +0800

    feat:去除公共模版，无需下载npm模块

--- generator.js
+++ generator.js
@@ -57,60 +57,60 @@ async function generate(dir, files, base = '', rootOptions = {}) {
 module.exports = (api, options, rootOptions) => {
 	
 	//扩展package配置
-	api.extendPackage(pkg => {
-		return {
-			dependencies: {
-				"axios": "^0.19.2",
-				"core-js": "^3.6.5",
-				"vue": "^2.6.11",
-				"vue-router": "^3.2.0",
-				"vuex": "^3.4.0"
-			},
-			devDependencies: {
-				"@vue/cli-plugin-babel": "^4.5.0",
-				"@vue/cli-service": "^4.5.0",
-				"babel-plugin-import": "^1.13.0",
-				"less": "^3.0.4",
-				"less-loader": "^5.0.0",
-				"vue-template-compiler": "^2.6.11"
-			}
-		}
-	});
+	// api.extendPackage(pkg => {
+	// 	return {
+	// 		dependencies: {
+	// 			"axios": "^0.19.2",
+	// 			"core-js": "^3.6.5",
+	// 			"vue": "^2.6.11",
+	// 			"vue-router": "^3.2.0",
+	// 			"vuex": "^3.4.0"
+	// 		},
+	// 		devDependencies: {
+	// 			"@vue/cli-plugin-babel": "^4.5.0",
+	// 			"@vue/cli-service": "^4.5.0",
+	// 			"babel-plugin-import": "^1.13.0",
+	// 			"less": "^3.0.4",
+	// 			"less-loader": "^5.0.0",
+	// 			"vue-template-compiler": "^2.6.11"
+	// 		}
+	// 	}
+	// });
 
-	if (options.template === 'bwa/bwa-template-h5') {
-		api.extendPackage(pkg => {
-			return {
-				dependencies: {
-					"vant": "^2.10.3"
-				},
-				devDependencies: {
+	// if (options.template === 'bwa/bwa-template-h5') {
+	// 	api.extendPackage(pkg => {
+	// 		return {
+	// 			dependencies: {
+	// 				"vant": "^2.10.3"
+	// 			},
+	// 			devDependencies: {
 
-				}
-			}
-		})
-	} 
-	else if (options.template === 'bwa/bwa-template-news') {
-		api.extendPackage(pkg => {
-			return {
-				dependencies: {
-					"vue-i18n": "^8.18.2"
-				},
-				devDependencies: {
+	// 			}
+	// 		}
+	// 	})
+	// } 
+	// else if (options.template === 'bwa/bwa-template-news') {
+	// 	api.extendPackage(pkg => {
+	// 		return {
+	// 			dependencies: {
+	// 				"vue-i18n": "^8.18.2"
+	// 			},
+	// 			devDependencies: {
 
-				}
-			}
-		})
-	}
+	// 			}
+	// 		}
+	// 	})
+	// }
 
 	api.render(async function (files) {
 
 		Object.keys(files).forEach(name => { delete files[name] });
 
 		let _template = options.repo || options.template
-		let _base = 'src';
+		let _base = '';
 
 		//获取公共模版内容
-		await generate(path.resolve(__dirname, './template/common'), files);
+		// await generate(path.resolve(__dirname, './template/common'), files);
 	
         let _spinner = ora('模板下载中...')
         
